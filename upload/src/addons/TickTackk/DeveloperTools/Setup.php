17cc03f5e966c50749b1912c883f6521460040c9
ticktackk <ims3kar@gmail.com>
2018-02-02 20:18:09 +0530

    Fixed Setup.php

--- upload/src/addons/TickTackk/DeveloperTools/Setup.php
+++ upload/src/addons/TickTackk/DeveloperTools/Setup.php
@@ -16,11 +16,34 @@ class Setup extends AbstractSetup
 
 	public function installStep1()
     {
-        $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+        $this->schemaManager()->alterTable('xf_addon', function(Alter $table)
         {
             $table->addColumn('license', 'mediumtext')->setDefault('');
             $table->addColumn('gitignore', 'mediumtext')->setDefault('');
             $table->addColumn('readme_md', 'mediumtext')->setDefault('');
+        });
+    }
+
+    public function upgrade1000010Step1()
+    {
+        $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+        {
+            $table->dropColumns(['license', 'gitignore', 'readme_md']);
+        });
+
+        $this->schemaManager()->alterTable('xf_addon', function(Alter $table)
+        {
+            $table->addColumn('license', 'mediumtext')->setDefault('');
+            $table->addColumn('gitignore', 'mediumtext')->setDefault('');
+            $table->addColumn('readme_md', 'mediumtext')->setDefault('');
+        });
+    }
+
+    public function uninstallStep1()
+    {
+        $this->schemaManager()->alterTable('xf_addon', function(Alter $table)
+        {
+            $table->dropColumns(['license', 'gitignore', 'readme_md']);
         });
     }
 }
