2c3ef33f6f13cdf4f0347a5a57f4455507b5d8c8
ticktackk <ims3kar@gmail.com>
2018-02-03 21:36:56 +0530

    Fixed Setup.php again...

--- upload/src/addons/TickTackk/DeveloperTools/Setup.php
+++ upload/src/addons/TickTackk/DeveloperTools/Setup.php
@@ -18,24 +18,43 @@ class Setup extends AbstractSetup
     {
         $this->schemaManager()->alterTable('xf_addon', function(Alter $table)
         {
-            $table->addColumn('license', 'mediumtext')->setDefault('');
-            $table->addColumn('gitignore', 'mediumtext')->setDefault('');
-            $table->addColumn('readme_md', 'mediumtext')->setDefault('');
+            $table->addColumn('license', 'mediumtext');
+            $table->addColumn('gitignore', 'mediumtext');
+            $table->addColumn('readme_md', 'mediumtext');
         });
     }
 
     public function upgrade1000010Step1()
     {
-        $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+        if ($this->schemaManager()->columnExists('xf_user', 'license'))
         {
-            $table->dropColumns(['license', 'gitignore', 'readme_md']);
-        });
+            $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+            {
+                $table->dropColumns('license');
+            });
+        }
+
+        if ($this->schemaManager()->columnExists('xf_user', 'gitignore'))
+        {
+            $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+            {
+                $table->dropColumns('gitignore');
+            });
+        }
+
+        if ($this->schemaManager()->columnExists('xf_user', 'readme_md'))
+        {
+            $this->schemaManager()->alterTable('xf_user', function(Alter $table)
+            {
+                $table->dropColumns('readme_md');
+            });
+        }
 
         $this->schemaManager()->alterTable('xf_addon', function(Alter $table)
         {
-            $table->addColumn('license', 'mediumtext')->setDefault('');
-            $table->addColumn('gitignore', 'mediumtext')->setDefault('');
-            $table->addColumn('readme_md', 'mediumtext')->setDefault('');
+            $table->addColumn('license', 'mediumtext');
+            $table->addColumn('gitignore', 'mediumtext');
+            $table->addColumn('readme_md', 'mediumtext');
         });
     }
 
