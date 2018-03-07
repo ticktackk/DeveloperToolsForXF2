2fe866e67cb0b3045c65afba958f15105576c1f5
ticktackk <31729605+ticktackk@users.noreply.github.com>
2018-03-07 06:38:49 +0530

    Clean up

--- upload/src/addons/TickTackk/DeveloperTools/Listener.php
+++ upload/src/addons/TickTackk/DeveloperTools/Listener.php
@@ -3,21 +3,23 @@
 namespace TickTackk\DeveloperTools;
 
 use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Manager;
+use XF\Mvc\Entity\Structure;
 
 class Listener
 {
+    public static $modificationId;
+
     public static function XFEntityAddOn_entity_structure(/** @noinspection PhpUnusedParameterInspection */
-        \XF\Mvc\Entity\Manager $em, \XF\Mvc\Entity\Structure &$structure)
+        Manager $em, Structure &$structure)
     {
         $structure->columns['license'] = ['type' => Entity::STR, 'default' => ''];
         $structure->columns['gitignore'] = ['type' => Entity::STR, 'default' => ''];
         $structure->columns['readme_md'] = ['type' => Entity::STR, 'default' => ''];
     }
 
-    public static $modificationId;
-
-    public static function XFEntityTemplateModification_entity_post_save(\XF\Mvc\Entity\Entity $entity)
+    public static function XFEntityTemplateModification_entity_post_save(Entity $entity)
     {
-        \TickTackk\DeveloperTools\Listener::$modificationId = $entity->getEntityId();
+        self::$modificationId = $entity->getEntityId();
     }
 }
