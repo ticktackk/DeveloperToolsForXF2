5e2df01e3f267d27a5a834850f9d798efc7b3385
Xon <tazipper1@hotmail.com>
2018-09-24 05:04:08 +0800

    Show template modifications which are applying (or failing) for a template

---
+++ upload/src/addons/TickTackk/DeveloperTools/XF/Admin/Controller/Template.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace TickTackk\DeveloperTools\XF\Admin\Controller;
+
+use XF\Diff;
+use XF\Entity\TemplateModification;
+use XF\Mvc\ParameterBag;
+use XF\Mvc\Reply\View;
+
+/**
+ * Extends \XF\Admin\Controller\Template
+ */
+class Template extends XFCP_Template
+{
+    public function actionEdit(ParameterBag $params)
+    {
+        $reply = parent::actionEdit($params);
+
+        if ($reply instanceof View &&
+            ($template = $reply->getParam('template')))
+        {
+            /** @var \XF\Entity\Template $template */
+            $modifications = $this->finder('XF:TemplateModification')
+                                  ->where([
+                                              'type'     => $template->type,
+                                              'template' => $template->title,
+                                              //'enabled'  => 1
+                                          ])
+                                  ->whereAddOnActive()
+                                  ->order('execution_order')
+                                  ->total();
+
+            $reply->setParam('modificationCount', $modifications);
+        }
+
+        return $reply;
+    }
+
+    public function actionViewModifications(ParameterBag $params)
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        $template = $this->assertTemplateExists($params->template_id);
+
+        $reload = $this->filter('reload', 'bool');
+        $ids = null;
+        if ($reload)
+        {
+            $ids = $this->filter('id', 'array-uint', []);
+            $ids = \array_fill_keys($ids, true);
+        }
+
+
+        $status = null;
+        /** @var \XF\Repository\TemplateModification $templateModRepo */
+        $templateModRepo = $this->repository('XF:TemplateModification');
+        $modifications = $this->finder('XF:TemplateModification')
+                              ->where([
+                                          'type'     => $template->type,
+                                          'template' => $template->title,
+                                      ])
+                              ->whereAddOnActive()
+                              ->order('execution_order')
+                              ->fetch();
+
+        $filtered = $modifications->filter(function (TemplateModification $mod) use ($ids) {
+            if ($ids === null)
+            {
+                return $mod->enabled;
+            }
+
+            return isset($ids[$mod->modification_id]);
+        });
+        $filtered = $filtered->toArray();
+        /** @var TemplateModification[] $modifications */
+        $templateText = $templateModRepo->applyTemplateModifications($template->template, $filtered, $statuses);
+
+        $diff = new Diff();
+        $diffs = $diff->findDifferences($template->template, $templateText);
+
+        $statuses = array_map(function($status)
+        {
+            if (is_numeric($status))
+            {
+                return 'Match count:'.$status;
+            }
+            return $status;
+        }, $statuses);
+
+
+        $viewParams = [
+            'style'       => $template->Style,
+            'template'    => $template,
+            'diffs'       => $diffs,
+            'mods'        => $modifications->toArray(),
+            'activeMods'  => $filtered,
+            'status'      => $statuses,
+            '_xfWithData' => $this->filter('_xfWithData', 'bool'),
+        ];
+
+        return $this->view('TickTackk\DeveloperTools:Template\Modifications\Compare', 'developerTools_template_modifications_compare', $viewParams);
+    }
+}
