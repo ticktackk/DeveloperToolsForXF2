7752652b7b75dd150aa8640e64645ad40a3218dd
Fillip Hannisdal <fillip@dragonbyte-tech.com>
2018-11-16 21:33:03 +0000

    Update git-commit to use git-move to copy files

--- upload/src/addons/TickTackk/DeveloperTools/Cli/Command/Git/Commit.php
+++ upload/src/addons/TickTackk/DeveloperTools/Cli/Command/Git/Commit.php
@@ -51,8 +51,6 @@ class Commit extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output) : ? int
     {
-        /** @var QuestionHelper $helper */
-        $helper = $this->getHelper('question');
         $addOnId = $input->getArgument('id');
         $addOn = $this->checkEditableAddOn($addOnId, $error);
         if (!$addOn)
@@ -60,213 +58,30 @@ class Commit extends Command
             $output->writeln('<error>' . $error . '</error>');
             return 1;
         }
-
-        /** @var \TickTackk\DeveloperTools\XF\Entity\AddOn $addOnEntity */
-        $addOnEntity = $addOn->getInstalledAddOn();
-
-        $addOnDirectory = $addOn->getAddOnDirectory();
-        $ds = DIRECTORY_SEPARATOR;
-
-        $repoRoot = $addOnDirectory . $ds . '_repo';
-
-        $git = new GitRepository($repoRoot);
-        if (!$git->isInitialized())
-        {
-            $command = $this->getApplication()->find('ticktackk-devtools:git-init');
-            $childInput = new ArrayInput([
-                'command' => 'ticktackk-devtools:git-init',
-                'id' => $addOn->getAddOnId()
-            ]);
-            $command->run($childInput, $output);
-        }
-
-        try
-        {
-            $customSubDir = $git->config()->get('custom.subdir')->execute();
-            if (!empty($customSubDir))
-            {
-                $repoRoot .= ($ds . $customSubDir);
-            }
-        }
-        catch (GitException $e)
-        {
-        }
-
-        $uploadDirectory = $repoRoot . $ds . 'upload';
-
-        if (is_dir($repoRoot))
-        {
-            if (file_exists($uploadDirectory))
-            {
-                File::deleteDirectory($uploadDirectory);
-            }
-
-            foreach ($this->getFileIterator($repoRoot) as $file)
-            {
-                $path = $this->standardizePath($repoRoot, $file->getPathname());
-
-                if (strpos($path, '.git') === 0)
-                {
-                    continue;
-                }
-
-                unlink($file->getRealPath());
-            }
-        }
-
-        $uploadRoot = $repoRoot . $ds . 'upload';
-        $srcRoot = $uploadRoot . $ds . 'src' . $ds . 'addons' . $ds . $addOn->prepareAddOnIdForPath();
-
-        $filesIterator = $this->getFileIterator($addOnDirectory);
-        foreach ($filesIterator AS $file)
-        {
-            $path = $this->standardizePath($addOnDirectory, $file->getPathname());
-            if ($this->isPartOfExcludedDirectoryForRepo($path))
-            {
-                continue;
-            }
-
-            if (!$file->isDir())
-            {
-                File::copyFile($file->getPathname(), $srcRoot . $ds . $path, false);
-            }
-        }
-
-        $rootPath = \XF::getRootDirectory();
-        $filesRoot = $addOn->getFilesDirectory();
-        $developerOptions = $addOnEntity->DeveloperOptions;
-
-        if (!empty($developerOptions['parse_additional_files']))
-        {
-            /** @noinspection PhpUndefinedFieldInspection */
-            $additionalFiles = $addOn->additional_files;
-            foreach ((array)$additionalFiles AS $additionalFile)
-            {
-                $filePath = $filesRoot . $ds . $additionalFile;
-                if (file_exists($filePath))
-                {
-                    $root = $filesRoot;
-                }
-                else
-                {
-                    $filePath = $rootPath . $ds . $additionalFile;
-                    if (!file_exists($filePath))
-                    {
-                        continue;
-                    }
-                    $root = $rootPath;
-                }
-
-                if (is_dir($filePath))
-                {
-                    $filesIterator = $this->getFileIterator($filePath);
-                    foreach ($filesIterator AS $file)
-                    {
-                        $stdPath = $this->standardizePath($root, $file->getPathname());
-                        if (!$file->isDir())
-                        {
-                            File::copyFile($file->getPathname(), $uploadRoot . $ds . $stdPath, false);
-                        }
-                    }
-                } else
-                {
-                    $stdPath = $this->standardizePath($root, $filePath);
-                    File::copyFile($filePath, $uploadRoot . $ds . $stdPath, false);
-                }
-            }
-        }
-
-        if (!empty($developerOptions['license']))
-        {
-            $licenseFileInRepoRoot = $repoRoot . $ds . 'LICENSE.md';
-            if (file_exists($licenseFileInRepoRoot) && is_readable($licenseFileInRepoRoot))
-            {
-                unlink($licenseFileInRepoRoot);
-            }
-
-            File::writeFile($repoRoot . $ds . 'LICENSE.md', $developerOptions['license'], false);
-        }
-
-        if (!empty($developerOptions['readme']))
-        {
-            $readMeMarkdownFileInRepoRoot = $repoRoot . $ds . 'README.md';
-            if (file_exists($readMeMarkdownFileInRepoRoot) && is_readable($readMeMarkdownFileInRepoRoot))
-            {
-                unlink($readMeMarkdownFileInRepoRoot);
-            }
-
-            File::writeFile($readMeMarkdownFileInRepoRoot, $developerOptions['readme'], false);
-        }
-
-        $globalGitIgnore = \XF::app()->options()->developerTools_git_ignore;
-        $globalGitIgnore = explode("\n", $globalGitIgnore);
-        $addOnGitIgnore = !empty($developerOptions['gitignore']) ? explode("\n", $developerOptions['gitignore']) : [];
-        $gitIgnoreLines = array_unique(array_merge($globalGitIgnore, $addOnGitIgnore));
-
-        File::writeFile($repoRoot . $ds . '.gitignore', implode("\n", $gitIgnoreLines), false);
-
-        $git->rm()->cached()->force()->execute('*');
-        $git->add()->execute('*');
-
-        $gitConfigurations = $addOnEntity->GitConfigurations;
-        if (empty($gitConfigurations['name']) || empty($gitConfigurations['email']))
-        {
-            $options = \XF::app()->options();
-            $globalGitName = $options->developerTools_git_username;
-            $globalGitEmail = $options->developerTools_git_email;
-
-            if (!empty($globalGitName) && !empty($globalGitEmail))
-            {
-                $gitConfigurations['name'] = $options->developerTools_git_username;
-                $gitConfigurations['email'] = $options->developerTools_git_email;
-            }
-            else
-            {
-                $output->writeln(['', 'No git username or email specified for ' . $addOnEntity->title]);
-                return 1;
-            }
-        }
-
-        try
-        {
-            $existingGitName = $git->config()->get('user.name')->execute();
-            if ($existingGitName !== $gitConfigurations['name'])
-            {
-                $git->config()->replaceAll('user.name', $gitConfigurations['name']);
-            }
-        }
-        catch (GitException $e)
-        {
-            $git->config()->add('user.name', $gitConfigurations['name'])->execute();
-        }
-
-        try
-        {
-            $existingGitEmail = $git->config()->get('user.email')->execute();
-            if ($existingGitEmail !== $gitConfigurations['email'])
-            {
-                $git->config()->replaceAll('user.email', $gitConfigurations['email']);
-            }
-        }
-        catch (GitException $e)
-        {
-            $git->config()->add('user.email', $gitConfigurations['email'])->execute();
-        }
-
-        try
-        {
-            if (empty($git->config()->get('user.name')->execute()) || empty($git->config()->get('user.email')->execute()))
-            {
-                $output->writeln(['', 'Git username or email cannot be empty.']);
-                return 1;
-            }
-        }
-        catch (GitException $e)
-        {
-            $output->writeln(['', $e->getMessage()]);
-            return 1;
-        }
-
+	
+		$command = $this->getApplication()->find('ticktackk-devtools:git-move');
+		$childInput = new ArrayInput([
+			'command' => 'ticktackk-devtools:git-move',
+			'id' => $addOn->getAddOnId()
+		]);
+		$command->run($childInput, $output);
+	
+		$addOnDirectory = $addOn->getAddOnDirectory();
+		$ds = DIRECTORY_SEPARATOR;
+	
+		$repoRoot = $addOnDirectory . $ds . '_repo';
+	
+		$git = new GitRepository($repoRoot);
+		if (!$git->isInitialized())
+		{
+			$command = $this->getApplication()->find('ticktackk-devtools:git-init');
+			$childInput = new ArrayInput([
+				'command' => 'ticktackk-devtools:git-init',
+				'id' => $addOn->getAddOnId()
+			]);
+			$command->run($childInput, $output);
+		}
+		
         if (empty($git->status()->getIndexStatus()))
         {
             $output->writeln(['', 'Nothing to commit.']);
@@ -276,7 +91,10 @@ class Commit extends Command
         $commitMessage = $input->getOption('message');
         if (!$commitMessage)
         {
-            $question = new Question('<question>Commit summary:</question>');
+			/** @var QuestionHelper $helper */
+			$helper = $this->getHelper('question');
+	
+			$question = new Question('<question>Commit summary:</question> ');
             $commitMessage = $helper->ask($input, $output, $question);
             $output->writeln('');
         }
