e549c8847a13506ed8a53550d7fb6842d8a03fff
TickTackk <me@ticktackk.io>
2019-02-09 16:46:20 +0530

    Remove code event listeners not required anymore

--- upload/src/addons/TickTackk/DeveloperTools/Listener.php
+++ upload/src/addons/TickTackk/DeveloperTools/Listener.php
@@ -21,31 +21,4 @@ class Listener
     {
         self::$modificationId = $entity->getEntityId();
     }
-
-    /**
-    * @param \XF\App $app
-    */
-    public static function appSetup(\XF\App $app) : void
-    {
-        Composer::autoloadNamespaces($app);
-        Composer::autoloadPsr4($app);
-        Composer::autoloadClassmap($app);
-        Composer::autoloadFiles($app);
-    }
-	
-    /**
-     * @param \XF\Cli\App $app
-     */
-    public static function appCliSetup(\XF\Cli\App $app) : void
-    {
-        $app->container()->factory('seed', function($class, array $params, Container $c) use ($app)
-        {
-            $class = \XF::stringToClass($class, '\%s\Seed\%s');
-            $class = $app->extendClass($class);
-
-            array_unshift($params, $app);
-
-            return $c->createObject($class, $params);
-        }, false);
-    }
 }
