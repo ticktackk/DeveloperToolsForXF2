98f80aacfa2578cab2730d45913f38b6ad055e72
TickTackk <me@ticktackk.io>
2019-04-28 02:13:10 +0530

    Show template modification type in breadcrumb

--- upload/src/addons/TickTackk/DeveloperTools/XF/Entity/TemplateModification.php
+++ upload/src/addons/TickTackk/DeveloperTools/XF/Entity/TemplateModification.php
@@ -3,6 +3,7 @@
 namespace TickTackk\DeveloperTools\XF\Entity;
 
 use TickTackk\DeveloperTools\App;
+use XF\Mvc\Entity\Structure;
 
 /**
  * Class TemplateModification
@@ -11,10 +12,32 @@ use TickTackk\DeveloperTools\App;
  */
 class TemplateModification extends XFCP_TemplateModification
 {
+    /**
+     * @return \XF\Phrase
+     */
+    public function getTypePhrase() : \XF\Phrase
+    {
+        return \XF::phrase($this->type);
+    }
+
     protected function _postSave()
     {
         parent::_postSave();
 
         App::$modificationId = $this->modification_id;
+    }
+
+    /**
+     * @param Structure $structure
+     *
+     * @return Structure
+     */
+    public static function getStructure(Structure $structure)
+    {
+        $structure = parent::getStructure($structure);
+
+        $structure->getters['TypePhrase'] = true;
+
+        return $structure;
     }
 }
