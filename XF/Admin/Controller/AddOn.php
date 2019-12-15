95156305bdf773fecc32b9217dabc4fe88b18a90
TickTackk <me@ticktackk.io>
2019-12-15 19:00:10 +0530

    Remove developer options action from AddOn controller

--- XF/Admin/Controller/AddOn.php
+++ XF/Admin/Controller/AddOn.php
@@ -2,6 +2,7 @@
 
 namespace TickTackk\DeveloperTools\XF\Admin\Controller;
 
+use XF\Mvc\Entity\Entity;
 use XF\Mvc\ParameterBag;
 use XF\Mvc\Reply\Exception as ExceptionReply;
 use XF\Mvc\Reply\Redirect as RedirectReply;
@@ -45,41 +46,5 @@ class AddOn extends XFCP_AddOn
         ];
 
         return $this->view('TickTackk\DeveloperTools\XF:AddOn\Build', '', $viewParams);
-    }
-
-    /**
-     * @param ParameterBag $parameterBag
-     *
-     * @return RedirectReply|ViewReply
-     * @throws ExceptionReply
-     */
-    public function actionDeveloperOptions(ParameterBag $parameterBag)
-    {
-        /** @noinspection PhpUndefinedFieldInspection */
-        $addOn = $this->assertAddOnAvailable($parameterBag->addon_id_url);
-
-        if (!$addOn->canEdit())
-        {
-            return $this->noPermission();
-        }
-
-        /** @var \TickTackk\DeveloperTools\XF\Repository\AddOn $addOnRepo */
-        $addOnRepo = $this->repository('XF:AddOn');
-
-        if ($this->isPost())
-        {
-            $developerOptions = $this->filter('dev', 'array');
-            $gitConfiguration = $this->filter('git', 'array');
-
-            $addOnRepo->exportDeveloperOptions($addOn->getInstalledAddOn(), $developerOptions);
-            $addOnRepo->exportGitConfiguration($addOn->getInstalledAddOn(), $gitConfiguration);
-
-            return $this->redirect($this->buildLink('add-ons/developer-options', $addOn->getInstalledAddOn()));
-        }
-
-        $viewParams = [
-            'addOn' => $addOn
-        ];
-        return $this->view('TickTackk\DeveloperTools\XF:AddOn\DeveloperOptions', 'developerTools_developer_options', $viewParams);
     }
 }
