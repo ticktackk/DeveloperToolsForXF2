543feccc4d91d5d2175b0e7ae88cf5907dec7d36
ticktackk <me@ticktackk.io>
2020-12-07 07:41:38 +0530

    Some long awaited compatibility fixes - take 2

--- XF/Mail/Mailer.php
+++ XF/Mail/Mailer.php
@@ -2,46 +2,14 @@
 
 namespace TickTackk\DeveloperTools\XF\Mail;
 
-use TickTackk\DeveloperTools\Repository\EmailLog as EmailLogLog;
-use \Swift_Mime_SimpleMessage as SwiftMimeSimpleMessage;
-use \Swift_Mime_Message as SwiftMimeMessage;
-use \Swift_Transport as SwiftTransport;
+$destClass = 'TickTackk\DeveloperTools\XF\Mail\Mailer';
+$srcClass = \XF::$versionId < 2020000
+    ? 'TickTackk\DeveloperTools\XF\Mail\XF2\Mailer'
+    : 'TickTackk\DeveloperTools\XF\Mail\XF22\Mailer';
 
-\class_alias(\XF::$versionId < 2020010 ? SwiftMimeMessage::class : SwiftMimeSimpleMessage::class, '\FinalSwiftMimeMessage');
-
-/**
- * Class Mailer
- * 
- * Extends \XF\Mail\Mailer
- *
- * @package TickTackk\DeveloperTools\XF\Mail
- */
-class Mailer extends XFCP_Mailer
-{
-    /**
-     * @param \FinalSwiftMimeMessage $message
-     * @param SwiftTransport|null $transport
-     * @param array|null $queueEntry
-     * @param bool $allowRetry
-     *
-     * @return int
-     *
-     * @throws \XF\PrintableException
-     *
-     * @noinspection PhpSignatureMismatchDuringInheritanceInspection
-     * @noinspection PhpUndefinedClassInspection
-     */
-    public function send(\FinalSwiftMimeMessage $message, SwiftTransport $transport = null, array $queueEntry = null, $allowRetry = true)
-    {
-        $sent = parent::send($message, $transport, $queueEntry, $allowRetry);
-
-        if ($sent)
-        {
-            /** @var EmailLogLog $emailLogRepo */
-            $emailLogRepo = \XF::app()->repository('TickTackk\DeveloperTools:EmailLog');
-            $emailLogRepo->log($message);
-        }
-
-        return $sent;
-    }
-}
+\class_alias($srcClass, $destClass);
+\class_alias(
+    'TickTackk\DeveloperTools\XF\Mail\XFCP_Mailer',
+    \XF::$versionId < 2020000 ? 'TickTackk\DeveloperTools\XF\Mail\XF22\XFCP_Mailer' : 'TickTackk\DeveloperTools\XF\Mail\XF2\XFCP_Mailer',
+    false
+);
