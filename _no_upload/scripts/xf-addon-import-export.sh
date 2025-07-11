15d95056ac1bbca5b1b058604874ad9e3358b7af
Xon <635541+Xon@users.noreply.github.com>
2025-07-11 09:21:48 +0800

    Add additional xf-dev:rebuild-caches calls for import/export scripts

--- _no_upload/scripts/xf-addon-import-export.sh
+++ _no_upload/scripts/xf-addon-import-export.sh
@@ -14,5 +14,6 @@ php cmd.php xf-dev:rebuild-caches -v || exit 1
 php cmd.php xf-dev:metadata-clean -v || exit 1
 php cmd.php tck-devtools:clamp-versions "$1" -v || exit 1
 php cmd.php xf-dev:import --addon="$1" -v || exit 1
+php cmd.php xf-dev:rebuild-caches -v || exit 1
 php cmd.php tck-devtools:clamp-versions "$1" -v || exit 1
 php cmd.php xf-dev:export --addon="$1" -v || exit 1
--- _no_upload/scripts/xf-addon-import.sh
+++ _no_upload/scripts/xf-addon-import.sh
@@ -13,3 +13,4 @@ fi
 php cmd.php xf-dev:metadata-clean -v || exit 1
 php cmd.php tck-devtools:clamp-versions "$1" -v || exit 1
 php cmd.php xf-dev:import --addon="$1" -v || exit 1
+php cmd.php xf-dev:rebuild-caches -v || exit 1
